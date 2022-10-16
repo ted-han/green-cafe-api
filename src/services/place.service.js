785f70389b7d30879a34433c1c206ae00bf517f3
ted-han <taekyung28h@gmail.com>
2022-10-16 18:14:52 +0900

    권한 없는경우 km 확인 안함

--- src/services/place.service.js
+++ src/services/place.service.js
@@ -3,7 +3,36 @@ const client = require("../db");
 async function placeList(ctx, next) {
   try {
     const { latitude, longitude, offset, limit } = ctx.query;
-    const sql = `
+    let res;
+    let sql;
+    // 권한이 없거나 현재위치 정보 조회에 실패한 경우
+    if (latitude === 37.497952 && longitude === 127.027619) {
+      sql = `
+      select
+        p.place_id
+        ,p.name
+        ,p.address
+        ,pi.url
+        ,pt.tag_name
+      FROM place p
+      LEFT OUTER JOIN (
+        select place_id, url
+        from place_image
+        where img_id = 1
+        ) pi
+      ON p.place_id = pi.place_id
+      LEFT OUTER JOIN (
+        select place_id, json_agg(tag_name) as tag_name
+        from place_tag
+        group by place_id
+        ) pt
+      ON p.place_id = pt.place_id
+      order by place_id asc
+      limit ${limit}
+      offset ${offset};
+    `;
+    } else {
+      sql = `
         select
           p.place_id
           ,p.name
@@ -28,7 +57,8 @@ async function placeList(ctx, next) {
         limit ${limit}
         offset ${offset};
       `;
-    const res = await client.query(sql);
+    }
+    res = await client.query(sql);
     ctx.body = res.rows;
   } catch (error) {
     console.log(`placeList: ${error}`);
